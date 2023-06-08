76362820076ae64f050d27a8d1ac1c8ecf920c7c
gabriel.alonso <gabriel.alonso@ross.com.ar>
2023-06-08 00:28:54 -0300

    se convierten a checkbox y se agregan preguntas con 2 opciones verdaderas

--- src/app/preguntas/preguntas.component.ts
+++ src/app/preguntas/preguntas.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 interface Pregunta {
   pregunta: string;
   opciones: string[];
-  respuesta: string[];
 }
 
 @Component({
@@ -11,89 +10,73 @@ interface Pregunta {
   templateUrl: './preguntas.component.html',
   styleUrls: ['./preguntas.component.css']
 })
-export class PreguntasComponent implements OnInit {
+export class PreguntasComponent {
+  preguntasSeleccionadas: Pregunta[] = [];
+  preguntasAleatorias: Pregunta[] = [];
+  preguntaActual: number = 0;
+  respuestasSeleccionadas: string[][] = [];
+  mostrarRespuestaIncorrecta: boolean = false;
+  mostrarSiguiente: boolean = false;
+  mostrarPreguntas: boolean = true;
+  preguntasCompletadas: boolean = false;
+  mostrarGracias: boolean = false;
+  mostrarPreguntaVolver: boolean = true;
+
   preguntas: Pregunta[] = [
     {
       pregunta: '¿Cuál es la capital de Francia?',
-      opciones: ['Londres', 'París', 'Madrid', 'Roma'],
-      respuesta: ['París']
-    },
-    {
-      pregunta: '¿En qué continente se encuentra Australia?',
-      opciones: ['Europa', 'Asia', 'África', 'Oceanía'],
-      respuesta: ['Oceanía']
+      opciones: ['*París', ' Madrid', ' Londres', ' Roma']
     },
     {
       pregunta: '¿Cuál es el río más largo del mundo?',
-      opciones: ['Amazonas', 'Nilo', 'Mississippi', 'Yangtsé'],
-      respuesta: ['Amazonas']
+      opciones: ['*Amazonas', ' Nilo', ' Yangtsé', ' Mississippi']
     },
     {
-      pregunta: '¿Cuál es el pecho frio mas grande?',
-      opciones: ['Belgrano', 'Instituto', 'Talleres', 'Racing'],
-      respuesta: ['Talleres']
+      pregunta: '¿Cuál es el pecho frio mas grande del futbol?',
+      opciones: ['*Talleres', ' Belgrano', ' Racing', ' Instituto']
+    },
+    {
+      pregunta: '¿Cuales son numeros pares (seleccionar 2 opciones)?',
+      opciones: ['*2', ' 5', ' 7', '*8']
+    },
+    {
+      pregunta: '¿Cuál es el océano más grande?',
+      opciones: ['*Pacífico', ' Atlántico', ' Indico', ' Antártico']
     }
   ];
 
-  preguntasAleatorias: Pregunta[] = [];
-  preguntasSeleccionadas: Pregunta[] = [];
-  preguntaActual = 0;
-  respuestasSeleccionadas: string[][] = [];
-  mostrarRespuestaIncorrecta = false;
-  mostrarSiguiente = false;
-  mostrarPreguntas = true;
-  preguntasCompletadas = false;
-  mostrarGracias = false;
-  mostrarPreguntaVolver = true;
-
-  ngOnInit() {
+  constructor() {
     this.preguntasAleatorias = this.obtenerPreguntasAleatorias();
     this.preguntasSeleccionadas = this.preguntasAleatorias.slice(0, 3);
     this.respuestasSeleccionadas = new Array(this.preguntasSeleccionadas.length).fill([]);
   }
 
   obtenerPreguntasAleatorias(): Pregunta[] {
-    const preguntas = [...this.preguntas];
-    const preguntasAleatorias: Pregunta[] = [];
-
-    while (preguntasAleatorias.length < 3) {
-      const index = Math.floor(Math.random() * preguntas.length);
-      const pregunta = preguntas.splice(index, 1)[0];
-      pregunta.opciones = this.obtenerOpcionesAleatorias(pregunta.opciones);
-      preguntasAleatorias.push(pregunta);
-    }
-
-    return preguntasAleatorias;
-  }
-
-  obtenerOpcionesAleatorias(opciones: string[]): string[] {
-    const opcionesAleatorias = [...opciones];
-
-    for (let i = opcionesAleatorias.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [opcionesAleatorias[i], opcionesAleatorias[j]] = [opcionesAleatorias[j], opcionesAleatorias[i]];
-    }
-
-    return opcionesAleatorias;
+    return this.preguntas.sort(() => Math.random() - 0.5);
   }
 
   verificarRespuestas(pregunta: Pregunta): boolean {
-    const respuestasSeleccionadas = this.respuestasSeleccionadas[this.preguntaActual];
-    return pregunta.respuesta.every(respuesta => respuestasSeleccionadas.includes(respuesta));
+    const respuestasObligatorias = pregunta.opciones.filter(opcion => opcion.startsWith('*'));
+    const respuestasSeleccionadas = this.respuestasSeleccionadas[this.preguntaActual] || [];
+    return (
+      respuestasObligatorias.length === respuestasSeleccionadas.length &&
+      respuestasObligatorias.every(opcion => respuestasSeleccionadas.includes(opcion))
+    );
   }
 
   siguientePregunta() {
-    this.mostrarRespuestaIncorrecta = false;
     this.preguntaActual++;
+    this.mostrarSiguiente = false;
+    this.mostrarRespuestaIncorrecta = false;
 
-    if (this.preguntaActual >= this.preguntasSeleccionadas.length) {
+    if (this.preguntaActual === this.preguntasSeleccionadas.length) {
       this.mostrarPreguntas = false;
       this.preguntasCompletadas = true;
     }
   }
 
   seleccionarRespuesta(opcion: string) {
-    const respuestasSeleccionadas = [...this.respuestasSeleccionadas[this.preguntaActual]];
+    const respuestasSeleccionadas = [...(this.respuestasSeleccionadas[this.preguntaActual] || [])];
 
     if (respuestasSeleccionadas.includes(opcion)) {
       const index = respuestasSeleccionadas.indexOf(opcion);
@@ -110,8 +93,8 @@ export class PreguntasComponent implements OnInit {
   reiniciarPrueba() {
     this.preguntasAleatorias = this.obtenerPreguntasAleatorias();
     this.preguntasSeleccionadas = this.preguntasAleatorias.slice(0, 3);
-    this.preguntaActual = 0;
     this.respuestasSeleccionadas = new Array(this.preguntasSeleccionadas.length).fill([]);
+    this.preguntaActual = 0;
     this.mostrarRespuestaIncorrecta = false;
     this.mostrarSiguiente = false;
     this.mostrarPreguntas = true;
@@ -120,13 +103,13 @@ export class PreguntasComponent implements OnInit {
     this.mostrarPreguntaVolver = true;
   }
 
-  volver() {
-    this.preguntaActual--;
-    this.mostrarSiguiente = true;
-    this.mostrarRespuestaIncorrecta = false;
+  volverInicio() {
+    this.mostrarGracias = false;
+    this.mostrarPreguntaVolver = true;
+  }
 
-    if (this.preguntaActual === 0) {
-      this.mostrarPreguntaVolver = true;
-    }
+  mostrarResultado() {
+    this.mostrarPreguntaVolver = false;
+    this.mostrarGracias = true;
   }
 }
